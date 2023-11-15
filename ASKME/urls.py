6c3c06a0aab556bb95c23261bdda25ed0e759ced
Ludik <danya.lubimov@yandex.ru>
2023-11-15 05:19:42 +0300

    DZ2

--- ASKME/urls.py
+++ ASKME/urls.py
@@ -21,8 +21,11 @@ from app import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('question/<int:question_id>', views.question, name="question"),
-    path('authorization', views.authorization, name="authorization"),
-    path('new_question', views.new_question, name="new_question"),
+    path('login', views.authorization, name="authorization"),
+    path('ask', views.new_question, name="new_question"),
     path('my_profile', views.my_profile, name="my_profile"),
+    path('signup', views.registration, name="registration"),
+    path('hot', views.hot_questions, name="hot_questions"),
+    path('tag/<str:tag_name>/', views.tag_questions, name="tag_questions"),
     path('admin/', admin.site.urls),
 ]
--- app/views.py
+++ app/views.py
@@ -1,35 +1,100 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.paginator import Paginator
+
+TAGS = ['Surfinging', 'Killepaking', 'Subheading']
+
 QUESTIONS = [
-        {
-            'id': i,
-            'title': f'Question {i}',
-            'content': f'Long lorus ipsum {i}'
-        } for i in range(20)
-    ]
+    {
+        'id': i,
+        'title': f'Question {i}',
+        'content': f'Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec '
+                   f'sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At '
+                   f'certam indicere. Cras mattis iudicium purus sit amet fermentum. {i} ',
+        'tag': TAGS[i % 3]
+    } for i in range(20)
+]
+
+HOT_QUESTIONS = [
+    {
+        'id': i,
+        'title': f'HotQuestion {i}',
+        'content': f'Hot Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec '
+                   f'sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At '
+                   f'certam indicere. Cras mattis iudicium purus sit amet fermentum. {i} '
+    } for i in range(20)
+]
 
 
-def paginate(objects, page,  per_page=15):
+def paginate(objects, request, per_page=15):
+    page = int(request.GET.get('page', 1))
+    if page > len(objects)//per_page+1:
+        page = len(objects)//per_page
     paginator = Paginator(objects, per_page)
     return paginator.page(page)
+
+
 def index(request):
-    page = request.GET.get('page', 1)
-    return render(request, 'index.html', {'questions': paginate(QUESTIONS, page)})
+    context = {
+        'questions': paginate(QUESTIONS, request),
+        'tags': TAGS
+    }
+    return render(request, 'index.html', context)
 
 
 def question(request, question_id):
     item = QUESTIONS[question_id]
-    return render(request, 'oneQuestion.html', {'question': item})
+    context = {
+        'question': item,
+        'tags': TAGS
+    }
+    return render(request, 'oneQuestion.html', context)
 
 
 def authorization(request):
-    return render(request, 'authorization.html')
+    context = {
+        'tags': TAGS
+    }
+    return render(request, 'authorization.html', context)
 
 
 def new_question(request):
-    return render(request, 'newquestion.html')
+    context = {
+        'tags': TAGS
+    }
+    return render(request, 'newquestion.html', context)
 
 
 def my_profile(request):
-    return render(request, 'profile.html')
+    context = {
+        'tags': TAGS
+    }
+    return render(request, 'profile.html', context)
+
+
+def registration(request):
+    context = {
+        'tags': TAGS
+    }
+    return render(request, 'registration.html', context)
+
+
+def hot_questions(request):
+    context = {
+        'questions': paginate(HOT_QUESTIONS, request),
+        'tags': TAGS
+    }
+    return render(request, 'index.html', context)
+
+
+def tag_questions(request, tag_name):
+    tag = tag_name
+    if tag != '':
+        objects = [item for item in QUESTIONS if item['tag'] == tag]
+    else:
+        objects = QUESTIONS
+    context = {
+        'questions': paginate(HOT_QUESTIONS, request),
+        'tags': TAGS
+    }
+    return render(request, 'tagQuestions.html', context)
